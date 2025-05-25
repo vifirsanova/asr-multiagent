70150bf60eff1dce31369000926b1a846acb3c6a
missvector <vifirsanova@gmail.com>
2025-05-25 19:13:52 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,13 +1,107 @@
-import argparse
+import argparse, re
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from transformers import AutoTokenizer, BitsAndBytesConfig, Gemma3ForCausalLM
 
 parser = argparse.ArgumentParser(description="Инструмент для оценки качества стихотворений на основе базы знаний")
-
 parser.add_argument("--data", type=str, required=True, help="Путь к хранилищу с тестовыми аудиозаписями")
-
 args = parser.parse_args()
-
 path = args.data
 
+"""
+WHISPER MODULE DEMO
+"""
+
+# Выбираем девайс: графический либо центральный процессор
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+# Выбираем модель Whisper из хаба HuggingFace
+model_id = "openai/whisper-large-v3"
+
+# Загружаем модель и отправляем ее на девайс
+model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+)
+model.to(device)
+
+# Процессор - это "токенизатор" для аудио-моделей
+processor = AutoProcessor.from_pretrained(model_id)
+
+# Пайплайн - это сборка для распознавания речи
+pipe = pipeline(
+    "automatic-speech-recognition", # Тип задачи
+    model=model, # Модель
+    tokenizer=processor.tokenizer, # Токенизатор
+    feature_extractor=processor.feature_extractor, # Извлечение признаков из спектрограмм
+    torch_dtype=torch_dtype, # Тип тензоров для обработки данных
+    device=device, # Девайс
+    return_timestamps=True, # Временные метки
+    # language='en' # Выбрать язык для перевода
+)
+
+prompt = pipe('audio.wav')['text'].strip()
+
+"""
+LLM MODULE DEMO
+"""
+
+model_id = "google/gemma-3-1b-it"
+# Применяем квантизацию: загружаем модель меньшей размерности
+quantization_config = BitsAndBytesConfig(load_in_8bit=True)
+
+# Инициализация модели из HuggingFace: загружается локально на наше устройство
+# Это значит, что она не использует сторонние сервисы, а все вычисления выполняются у нас
+model = Gemma3ForCausalLM.from_pretrained(
+    model_id, quantization_config=quantization_config
+).eval()
+
+# Токенизация тоже производится локально, т.е. на нашем устройстве
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+# Определяем путь к файлу с промптом на нужном языке
+prompt_path ='prompts/meta_prompt.txt'
+
+# Открываем файл с нужным промптом
+with open(prompt_path) as f:
+    system_prompt = f.read()
+
+# Подгрузка базы данных из файла
+with open('data/database.json') as f:
+    database = f.read()
+
+# Системные роли удобнее подгружать из отдельного файла
+messages = [
+    [
+        {
+            "role": "system",
+            "content": [{"type": "text", "text": system_prompt+database},]
+        },
+        {
+            "role": "user",
+            "content": [{"type": "text", "text": f'USER QUERY: {prompt}'},]
+        },
+    ],
+]
+
+inputs = tokenizer.apply_chat_template(
+    messages,
+    add_generation_prompt=True,
+    tokenize=True,
+    return_dict=True,
+    return_tensors="pt",
+).to(model.device)
+
+with torch.inference_mode():
+    outputs = model.generate(**inputs, temperature=0.7, top_k=50, max_new_tokens=1024)
+
+outputs = tokenizer.batch_decode(outputs)
+
+# Добавляем парсинг ответа модели
+pattern = r'<start_of_turn>(.*?)<end_of_turn>'
+matches = re.findall(pattern, outputs[0], re.DOTALL)
+
+print(matches[1])
 """
 TODO: извлечение и предобработка данных с целью оптимизации для нейросетевой обработки (нейросеть много кушает, поэтому лучше сразу сжать все файлы) 
     - описать логику для парсинга каталога с аудиозаписями
